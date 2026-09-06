08b38ff3301bfd82a7410e2e9160191c377895f5
Lakkshman Ajay B <136235261+lakkshman10@users.noreply.github.com>
2026-09-07 00:58:27 +0530

    fix: separate user account from financial accounts

--- finance-tracker/src/App.js
+++ finance-tracker/src/App.js
@@ -54,7 +54,6 @@ function MainContent({ isSidebarCollapsed, toggleSidebar }) {
     '/income-tracking',
     '/reports-insights',
     '/accounts',
-    '/account',
   ];
 
   const showSidebar = routesWithSidebar.includes(location.pathname);
@@ -87,7 +86,6 @@ function MainContent({ isSidebarCollapsed, toggleSidebar }) {
             <Route path="/income-tracking" element={<ProtectedRoute element={IncomeTracking} />} />
             <Route path="/reports-insights" element={<ProtectedRoute element={ReportsInsights} />} />
             <Route path="/accounts" element={<ProtectedRoute element={Accounts} />} />
-            <Route path="/account" element={<ProtectedRoute element={Accounts} />} />
           </Routes>
         </div>
       </div>
